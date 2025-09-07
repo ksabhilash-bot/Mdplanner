7607be59a0d33a3a90be9f05b835efb4180393d0
Amal <Amalkrishnanvp17@gmail.com>
2025-09-07 20:53:30 +0530

    Edited imports for deploying 4

--- client/src/components/common/ModeToggle.jsx
+++ client/src/components/common/ModeToggle.jsx
@@ -41,7 +41,7 @@
 // }
 
 import { Moon, Sun } from "lucide-react";
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import { useTheme } from "@/components/common/ThemeProvider";
 
 export function ModeToggle() {
